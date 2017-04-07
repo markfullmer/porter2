626336ce432870c995bea6eb588fb632bccb7dee
Fullmer <jmf3658@ITS-APP-889109.local>
2017-04-07 16:58:08 -0500

    Add static approach & test suite

---
+++ test/Basic1Test.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace markfullmer\porterstemmer;
+
+/**
+ * Test words are stemmed correctly.
+ */
+class Basic1Test extends \PHPUnit_Framework_TestCase {
+
+  /**
+   * Provides data.
+   */
+  public function basicDataProvider() {
+    $words = retrieve_stem_words(0, 5000);
+    $tests = array();
+    foreach ($words as $key => $values) {
+      $tests[$key]['word'] = $values[0];
+      $tests[$key]['expected'] = $values[1];
+    }
+    return $tests;
+  }
+
+  /**
+   * Test assertions.
+   *
+   * @dataProvider basicDataProvider
+   */
+  public function testBasic1($word, $expected) {
+    $stem = Porter2::stem($word);
+    $this->assertEquals($expected, $stem);
+  }
+
+}
